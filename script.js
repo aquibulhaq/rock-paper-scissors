de9b15322a7ff669151473cda0f1eb9169073c33
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-09-04 14:17:20 +0600

    Create playRound() function
    Also create the integerToString array.

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+const integerToString = ['Rock', 'Paper', 'Scissors'];
+
+function playRound(humanChoiceInt, computerChoiceInt) {
+  const humanChoiceStr = integerToString[humanChoiceInt];
+  const computerChoiceStr = integerToString[computerChoiceInt];
+
+  if (humanChoiceInt === computerChoiceInt) {
+    console.log(`Draw! ${humanChoiceStr} and ${computerChoiceStr} draw.`);
+  } else if ((computerChoiceInt + 1) % integerToString.length === humanChoiceInt) {
+    console.log(`You win! ${humanChoiceStr} beats ${computerChoiceStr}.`);
+    ++humanScore;
+  } else {
+    console.log(`You lose! ${computerChoiceStr} beats ${humanChoiceStr}.`);
+    ++computerScore;
+  }
+}
