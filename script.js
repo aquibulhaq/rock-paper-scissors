70cb4af74bb852759630f32f6a563d8f630d73d0
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-09-04 12:44:56 +0600

    Create getHumanChoice() function
    Also create the stringToInteger object.

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ console.log('Hello, world!');
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
+
+const stringToInteger = {
+  rock: 0,
+  paper: 1,
+  scissors: 2
+};
+
+function getHumanChoice() {
+  let choice;
+
+  do {
+    choice = prompt('Enter your choice (rock/paper/scissors):').toLowerCase();
+  } while (!(choice in stringToInteger));
+
+  return stringToInteger[choice];
+}
