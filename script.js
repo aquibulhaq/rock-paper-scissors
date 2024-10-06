7c0131bf07d63f7c0a5b93c290bf7545778b7390
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-10-06 20:50:24 +0600

    Refactor playGame() function for readability
    Put playRound(), the score variables, and the array integerToString
    inside playGame().

--- script.js
+++ script.js
@@ -20,27 +20,27 @@ function getHumanChoice() {
   return stringToInteger[choice];
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-const integerToString = ['Rock', 'Paper', 'Scissors'];
-
-function playRound(humanChoiceInt, computerChoiceInt) {
-  const humanChoiceStr = integerToString[humanChoiceInt];
-  const computerChoiceStr = integerToString[computerChoiceInt];
-
-  if (humanChoiceInt === computerChoiceInt) {
-    console.log(`Draw! ${humanChoiceStr} and ${computerChoiceStr} draw.`);
-  } else if ((computerChoiceInt + 1) % integerToString.length === humanChoiceInt) {
-    console.log(`You win! ${humanChoiceStr} beats ${computerChoiceStr}.`);
-    ++humanScore;
-  } else {
-    console.log(`You lose! ${computerChoiceStr} beats ${humanChoiceStr}.`);
-    ++computerScore;
-  }
-}
-
 function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  const integerToString = ['Rock', 'Paper', 'Scissors'];
+
+  function playRound(humanChoiceInt, computerChoiceInt) {
+    const humanChoiceStr = integerToString[humanChoiceInt];
+    const computerChoiceStr = integerToString[computerChoiceInt];
+
+    if (humanChoiceInt === computerChoiceInt) {
+      console.log(`Draw! ${humanChoiceStr} and ${computerChoiceStr} draw.`);
+    } else if ((computerChoiceInt + 1) % integerToString.length === humanChoiceInt) {
+      console.log(`You win! ${humanChoiceStr} beats ${computerChoiceStr}.`);
+      ++humanScore;
+    } else {
+      console.log(`You lose! ${computerChoiceStr} beats ${humanChoiceStr}.`);
+      ++computerScore;
+    }
+  }
+
   for (let i = 0; i < 5; ++i)
     playRound(getHumanChoice(), getComputerChoice());
 
